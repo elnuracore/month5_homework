0fa922646f2ada2c48fd1722e9cbc16e0d346d82
elnuracore <elnuraanarbaikyzy7@gmail.com>
2026-02-24 19:24:05 +0600

    month 6 hw1

--- users/models.py
+++ users/models.py
@@ -1,9 +1,21 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
-class UserConfirmation(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='confirmation')
-    code = models.CharField(max_length=6)
+class User(AbstractUser):
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+    def clean(self):
+        super().clean()
+        if self.is_superuser and not self.phone_number:
+            raise ValidationError(
+                {'phone_number': 'Номер телефона обязателен для суперпользователя.'}
+            )
 
     def __str__(self):
-        return f"{self.user} - {self.code}"
+        return self.username
+
+class UserConfirmation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    code = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
--- users/serializers.py
+++ users/serializers.py
@@ -3,6 +3,23 @@ from django.contrib.auth.models import User
 from rest_framework.exceptions import ValidationError
 from rest_framework.validators import UniqueValidator
 
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'phone_number', 'is_superuser']
+
+    def validate(self, data):
+        is_superuser = data.get('is_superuser', self.instance.is_superuser if self.instance else False)
+        phone = data.get('phone_number')
+
+        if is_superuser and not phone:
+            raise serializers.ValidationError(
+                {"phone_number": "Суперпользователь должен иметь номер телефона."}
+            )
+        return data
+
 class UserBaseValidation(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField()
